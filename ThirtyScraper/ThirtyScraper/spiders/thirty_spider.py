a561c2f7eaab8443fb1c5b761115d6b025b7c74b
Giovanni Ordonez <giovanniordonez33@gmail.com>
2021-01-01 15:18:33 -0600

    Practice
    I was able to get the source code of a website to try out the spider.

--- ThirtyScraper/ThirtyScraper/spiders/thirty_spider.py
+++ ThirtyScraper/ThirtyScraper/spiders/thirty_spider.py
@@ -1,11 +1,12 @@
-import scrapy 
+import scrapy
 
 class SuggestionsSpider(scrapy.Spider):
-	suggestions = "tasks"
-
-	scrape_urls = [
-		'http://somewebsite.com/user/suggestions'
-	]
+	name = "tasks"
+		
+	def start_requests(self):
+		scrape_urls = [
+			'https://www.brainyquote.com/topics/motivational-quotes'
+		]
 	
 	#by using yield we are creating a generator object which we can use to iterate. So here
 	#we are essentially just calling the request func from scrapy and then iterating it on the
@@ -16,16 +17,16 @@ class SuggestionsSpider(scrapy.Spider):
 	#stopping for each scrapy.Request to fully terminate before going to the next iteration of the
 	#urls. Hence we do not need to run through all the urls and use up a lot of memory space and
 	#run through them one by one which requires less memory! (yiels gives us a generator = iterable obj 
-	for url in scrape_urls:
-		yield  scrapy.Request(url=url, callback=self.parse)
+		for url in scrape_urls:
+			yield scrapy.Request(url=url, callback=self.parse)
 
 	#we parse the html source code of the location we scraped from which are the url links in the
 	#start url object we created! We then are creting a file to save that source code from which 
 	#we can then use to parse the data and see what we can find and hence be able to extract
 	#exactly the certain components that we need for making the task suggestions!
 	def parse(self, response):
-		page = response.url.splt("/")
-		filename = f'sugesstion-{page}.html'
+		page = response.url.split("/")[-2]
+		filename = f'tasks-{page}.html'
 		with open(filename, 'wb') as f:
 			f.write(response.body)
 

f5f77c59377c7d0aa72a62dfa7707d66189eee37
Giovanni Ordonez <giovanniordonez33@gmail.com>
2021-01-02 22:07:14 -0600

    Cleaning Code

--- ThirtyScraper/ThirtyScraper/spiders/thirty_spider.py
+++ ThirtyScraper/ThirtyScraper/spiders/thirty_spider.py
@@ -1,12 +1,13 @@
 import scrapy
-import pandas as pd
 from urllib.parse import urlencode
-from urllib.parse import urlparse
 import json
 from datetime import datetime
 import os
 
 API_KEY = os.getenv('API_KEY')
+
+# To run: scrapy crawl tasks -o test.csv
+# -o = ouput to the file test.csv for csv formated data
 
 """
 We are creating a proxy link that we can target our requests to. In reality
@@ -38,17 +39,14 @@ query
 
 def create_google_url(query, site=''):
     google_dictionary = {'q': query, 'num': 100}
-    if site:
-        web = urlparse(site).netloc
-        google_dictionary['as_sitesearch'] = web
-        return 'http://www.google.com/search?' + urlencode(google_dictionary)
+
     return 'http://www.google.com/search?' + urlencode(google_dictionary)
 
 
 class SuggestionsSpider(scrapy.Spider):
     name = 'tasks'
     allowed_domains = ['api.scraperapi.com']
-    #these settings are for the free Scaper API plan!
+    # these settings are for the free Scaper API plan!
     custom_settings = {'ROBOTSTXT_OBEY': False, 'LOG_LEVEL': 'INFO',
                        'CONCURRENT_REQUESTS_PER_DOMAIN': 10}
 
@@ -63,24 +61,32 @@ class SuggestionsSpider(scrapy.Spider):
         url = create_google_url('coffee+shops')
 
         yield scrapy.Request(get_url(url), callback=self.parse,
-                                 meta={'pos': 0})
+                             meta={'pos': 0})
 
+    """
+    We are now looping through the response object and extracting the data
+    we want to see. We are keeping track of the pages we see with the position
+    to count how many webpages we have scraped.
+
+    The item obj then holds all of the data we want for each single page and
+    we are then using that item to ouput to a csv file from where we can then
+    see the collection of pages with their data based on the way we parsed the
+    response obj in the parse() function
+    """
     def parse(self, response):
         data = json.loads(response.text)
         position = response.meta['pos']
-        data_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        queried_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         for res in data['organic_results']:
             title = res['title']
             snippet = res['snippet']
             link = res['link']
-            item = {'title': title, 'snippet': snippet, 'link': link, 'pos':
-                    position, 'date':  data_time}
+            web_page_data = {'title': title, 'snippet': snippet, 'link': link,
+                             'pos': position, 'date':  queried_time}
             position += 1
-            yield item
-
+            yield web_page_data
+        # If another page exists in the query search then get that page's data
         next_web_page = data['pagination']['nextPageUrl']
         if next_web_page:
             yield scrapy.Request(get_url(next_web_page), callback=self.parse,
                                  meta={'pos': position})
-
-
